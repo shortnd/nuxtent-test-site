1a7bfda5be1d194cee0438a44b9277940bb6fd1c
shortnd <ococncol@gmail.com>
2018-08-29 09:16:48 -0400

    trying another nuxtServerInit

--- components/HeaderNavbar.vue
+++ components/HeaderNavbar.vue
@@ -3,7 +3,7 @@
     <div class="top-bar">
       <a :href="'tel:' + header_info.phone_number">{{ header_info.phone_number }}</a>
       <a :href="'mailto:' + header_info.email_address">{{ header_info.email_address }}</a>
-      <a :href="'https://'+header_info.facebook_link" target="_blank">{{ header_info.facebook_link }}</a>
+      <a :href="'https://'+header_info.facebook_link" target="_blank">facebook</a>
     </div>
     <nav>
       <nuxt-link to="/">Home</nuxt-link>
--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 import Vuex from 'vuex'
-import axios from 'axios'
+
 const createStore = () => {
   return new Vuex.Store({
     state: {
@@ -11,14 +11,9 @@ const createStore = () => {
       }
     },
     actions: {
-      async nuxtServerInit ({commit}, {app}) {
-        if (process.env.NODE_ENV === 'production') {
-          let res = await app.$axios.$get(`${process.env.NODE_ENV}/content-api/site_info`)
-          commit('setSiteInfo', res)
-        } else {
-          let res = await app.$axios.$get(`/site_info`)
-          commit('setSiteInfo', res)
-        }
+      nuxtServerInit: async ({commit}, {app}) => {
+        let res = await app.$axios.$get(`/site_info`)
+        commit('setSiteInfo', res)
       }
     }
   })
